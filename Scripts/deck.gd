dbc8043cf652bb8735d68e386d02f2aa88d233a0
lyrhe <steamfake01@gmail.com>
2025-02-02 00:43:32 +0100

    Shuffler fixed

--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -34,6 +34,23 @@ func load_deck(deck_file_path: String) -> void:
 	for card_instance in cards:
 		deck_grid.add_child(card_instance, true)
 
+# Mélange le deck en récupérant les cartes actuellement dans le deck dans une liste, shuffle() puis repopulate
+func shuffle_deck():
+	var cards := []
+	for n in deck_grid.get_children():
+		cards.append(n.card_code)
+	cards.shuffle()
+	for n in deck_grid.get_children():
+		deck_grid.remove_child(n)
+		n.queue_free()
+	for n in cards:
+		var card_instance = card.instantiate()
+		var texture_path = "res://Assets/card_images/%s.png" % n
+		var texture = load(texture_path)
+		card_instance.texture = texture
+		card_instance.card_code = n
+		deck_grid.add_child(card_instance, true)
+
 # Ajoute une carte à la main du joueur avec un clic gauche sur le deck
 func _on_deck_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -41,3 +58,8 @@ func _on_deck_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -
 			player_hand.force_draw(1)
 	if Input.is_action_just_pressed("right_mouse_click"):
 		deck_grid.visible = not deck_grid.visible
+
+# Shuffle à chaque fois que le deck est ouvert
+func _on_deck_visibility_changed() -> void:
+	if deck_grid.visible == true:
+		shuffle_deck()
