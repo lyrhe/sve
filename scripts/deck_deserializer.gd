6defff064c7a4c4cf9936ca3582a40d6e8c7c74b
Thomas Guillouët <thomas.guillouet@outlook.com>
2025-02-21 23:13:00 +0100

    Updating the return type of load_cards_list

--- scripts/deck_deserializer.gd
+++ scripts/deck_deserializer.gd
@@ -1,6 +1,6 @@
 class_name DeckDeserializer extends Resource
 
-func load_cards_list(deck_path, database_path):
+func load_cards_list(deck_path, database_path) -> Array[Card]:
 	var deck:= FileAccess.open(deck_path, FileAccess.READ)
 	var database := FileAccess.open(database_path, FileAccess.READ)
 	var content = database.get_as_text()
